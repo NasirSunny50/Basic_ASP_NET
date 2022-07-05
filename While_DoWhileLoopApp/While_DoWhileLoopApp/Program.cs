28b11ef0dec627dcc1cea8ec06472386ba0d32fa
Nasir Sunny <63219166+NasirSunny50@users.noreply.github.com>
2022-07-05 14:46:34 +0600

    while and do while loop added

---
+++ While_DoWhileLoopApp/While_DoWhileLoopApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_DoWhileLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while Loop
+            //Even Odd Number
+            int number = 0;
+            while (number < 10)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+
+            //do while loop
+
+            int number1 = 0;
+
+            do
+            {
+                if(number1%2!=0)
+                {
+                    Console.WriteLine(number1);
+                }
+                number1++;
+            }
+            while (number1<20);
+
+            Console.ReadKey();
+        }
+    }
+}
