42bab2680c3907119f3eb770751bc5fd76aa9f18
Nasir Sunny <63219166+NasirSunny50@users.noreply.github.com>
2022-07-05 11:06:10 +0600

    if else for loop added

---
+++ IfElseForLoopApp/IfElseForLoopApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseForLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Even Odd Detection
+            Console.Write("Enter a Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            //print Even or Odd     
+
+            for(int i =0; i<number; i++)
+            {
+                if(i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
