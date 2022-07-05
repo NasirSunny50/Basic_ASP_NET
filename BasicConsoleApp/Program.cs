df6ca494711ef513aa70dfe8ea2cd037e505e964
Nasir Sunny <63219166+NasirSunny50@users.noreply.github.com>
2022-07-05 10:27:42 +0600

    Add project files.

---
+++ BasicConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Name:");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.Write("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(name);
+            Console.Write(age);
+
+            
+            Console.ReadKey();
+        }
+    }
+
+}
