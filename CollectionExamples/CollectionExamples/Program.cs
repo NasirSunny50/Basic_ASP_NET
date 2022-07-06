dfd8d3ec73d8894042c5af34df4ff77c273785d1
Nasir Sunny <63219166+NasirSunny50@users.noreply.github.com>
2022-07-06 11:38:36 +0600

    method intro added

---
+++ CollectionExamples/CollectionExamples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] num = new int[5];
+
+            ////populate the array
+
+            //for(int i=0; i<num.Length; i++)
+            //{
+            //    String userInputText = Console.ReadLine();
+            //    int userInput = Convert.ToInt32(userInputText);
+
+            //    num[i] = userInput;
+            //}
+
+            ////sum the array values
+
+            //int result = 0;
+
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    int num1 = num[i];
+
+            //    result = result + num1;
+
+            //}
+
+            int[] values = new[] { 1, 5, 3, 6, 8, 9 };
+
+            Array.Sort(values);
+
+            for(int i=0; i<values.Length; i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MethodIntroApp/MethodIntroApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodIntroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = substract(20, 60);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        static int substract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+
+    }
+}
